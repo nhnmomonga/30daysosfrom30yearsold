d5ddadae50a6930fa8aa55ceb960c4c202c52ff8
haruna nishiwaki <haruna.nishiwaki@gmail.com>
2019-12-04 00:31:33 +0900

    fix lib path info

--- helloos3/bootpack.c
+++ helloos3/bootpack.c
@@ -3,13 +3,13 @@
 void io_hlt(void);
 
 /* 関数宣言なのに、{}がなくていきなり;を書くと、
-	他のファイルにあるからよろしくね、という意味になるのです。 */
+    他のファイルにあるからよろしくね、という意味になるのです。 */
 
 void HariMain(void)
 {
 
 fin:
-	io_hlt(); /* これでnaskfunc.nasの_io_hltが実行されます */
-	goto fin;
+    io_hlt(); /* これでnaskfunc.nasの_io_hltが実行されます */
+    goto fin;
 
 }
